534008982053fc51fb51e295235e299441f023c0
Denis-Poloczek <denis.poloczek@icloud.com>
2022-10-18 19:02:27 +0200

    first commit

---
+++ class_data.py
@@ -0,0 +1,31 @@
+sorting_methods_dict = {1: "Selection Sort", 2: "insertion_sort" , 3: "Shell Sort",
+                        4: "Bubble Sort", 5: "Merge Sort", 6: "QuickSort", 7: "Bucket Sort"}
+
+sorting_methods_details_dict = {
+    "Selection Sort": " Θ(n^2) time complexity, one of the worst along all sorting methods",
+    "Insertion Sort": " Θ(n^2) time complexity, one of the worst along all sorting methods",
+    "Shell Sort": " Θ(n^2) time complexity, one of the worst along all sorting methods",
+    "Bubble Sort": " Θ(n^2) time complexity, one of the worst along all sorting methods",
+    "Merge Sort": " Θ(n^2) time complexity, one of the worst along all sorting methods",
+    "QuickSort": " Θ(n^2) time complexity, one of the worst along all sorting methods",
+    "Bucket Sort": " Θ(n^2) time complexity, one of the worst along all sorting methods"}
+
+searching_methods_dict = ["Linear Search", "Jump Search", "Binary Search",
+                          "Interpolation Search", "Exponential Search"]
+
+searching_methods_details_dict = {
+    "Linear Search": " Θ(n^2) time complexity, one of the worst along all sorting methods",
+    "Jump Search": " Θ(n^2) time complexity, one of the worst along all sorting methods",
+    "Binary Search": " Θ(n^2) time complexity, one of the worst along all sorting methods",
+    "Interpolation Search": " Θ(n^2) time complexity, one of the worst along all sorting methods",
+    "Exponential Search": " Θ(n^2) time complexity, one of the worst along all sorting methods",
+}
+
+ascii_art = """ 
+
+
+
+
+
+
+"""
